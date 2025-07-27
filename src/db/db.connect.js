900e7f5e4f45902e7532dfa6a66f3f1d9e2d08ee
Amit Kumar <amitraazec53@gmail.com>
2025-07-27 11:55:31 +0530

    ch

--- src/db/db.connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-require('dotenv').config()
-let db = "mongodb://13.50.201.122:27017/jenkinsDb"
-
-mongoose.connect(db).then(()=>{
-    console.log("database has to connect")
-},()=>{
-    console.log("database has not connected")
-})
